04116b02988773c17b82135ebec47dca4817977a
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2021-06-26 16:29:40 +0530

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,12 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100     # Initial height
+num_bounces = 0
+
+while num_bounces < 10:
+    height *= 3/5
+    num_bounces += 1
+    print(num_bounces, round(height, ndigits=4))
+    
