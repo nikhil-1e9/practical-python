9c49d1a443f71d52a75133454a8c5a106c8e05cd
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2021-06-26 16:34:42 +0530

    Update bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,7 +1,26 @@
 # bounce.py
 #
-# Exercise 1.5
+'''
+Exercise 1.5: The Bouncing Ball
 
+A rubber ball is dropped from a height of 100 meters and each time it hits the ground, it bounces back up to 3/5 the height it fell. Write a program that prints 
+a table showing the height of the first 10 bounces.
+
+Your program should make a table that looks something like this:
+
+1 60.0
+2 36.0
+3 21.6
+4 12.96
+5 7.776
+6 4.6656
+7 2.7994
+8 1.6796
+9 1.0078
+10 0.6047
+'''
+
+# Solution
 height = 100     # Initial height
 num_bounces = 0
 
