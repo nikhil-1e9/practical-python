1f7f884efa8c2728708ca5354ecfb32bf8c01690
nikhil-1e9 <84204563+nikhil-1e9@users.noreply.github.com>
2021-07-04 00:23:01 +0530

    Update pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -19,4 +19,34 @@ with open('Data/portfolio.csv', 'rt') as f:
         
 print(f'Total cost {total}')
 
+#
+'''
+Exercise 2.15: A practical enumerate() example
+
+Recall that the file Data/missing.csv contains data for a stock portfolio, but has some rows with missing data. Using enumerate(), modify your pcost.py program so that 
+it prints a line number with the warning message when it encounters bad input.
+'''
+
+def portfolio_cost(filename):
+    import csv
+    
+    total = 0.0
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        next(rows)
+        for rownum, row in enumerate(rows, start=1):
+            try:
+                shares = int(row[1])
+                price = float(row[2])
+            except ValueError:
+                print(f'Row {rownum}: Couldn\'t convert: {row}')
+            
+            total += shares * price
+        
+        return total
+
+
+cost = portfolio_cost('Data/missing.csv')
+
 
